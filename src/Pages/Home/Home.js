4914bd5dc19909dee8431df456844ec6ab2df937
Syma Nusrat <symanusrat158@gmail.com>
2022-05-06 15:46:50 +0600

    objectives added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Intro from './Intro/Intro';
 import Inventory from './Inventory/Inventory';
+import Objectives from './Objectives/Objectives';
 
 const Home = () => {
     return (
@@ -11,7 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <Intro></Intro>
             <Inventory></Inventory>
-            
+            <Objectives></Objectives>
             
 
         </div>
---
+++ src/Pages/Home/Objectives/Objectives.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Objectives = () => {
+  return (
+    <div>
+      <div>
+        <h2>Our goal and Objectives</h2>
+        <p>
+          Our objective is to Reduce our clients overall Logistics Cost and
+          Increase their Customer Order Servicability.
+        </p>
+        <p>
+          Well established and highly regarded in the electronics market. A
+          dedicated group of engineers working to give the highest quality.
+          Efficient production methods WEAKNESSES Building new relations with
+          suppliers. The target group can choose cheaper models if they want.
+          Low debt equity rating. Lower risk.
+        </p>
+      </div>
+      <div>
+          
+      </div>
+    </div>
+  );
+};
+
+export default Objectives;
