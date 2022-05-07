4669794091ff942f0df8f8bf598d77587e72188b
Syma Nusrat <symanusrat158@gmail.com>
2022-05-07 20:47:07 +0600

    verified mail

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from '../../../firebase.init';
-
-import google from '../../../images/Logo/google.png'
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {  useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import google from "../../../images/Logo/google.png";
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    return (
-        <div>
-            <div className='d-flex align-items-center'>
-                
-               <div style={{height:'1px'}} className='bg-primary w-50'></div>
-               <p className='mt-2 px-2'>or</p>
-               <div style={{height:'1px'}} className='bg-primary w-50'></div>
-                
-            </div>
-              {/* set button */}
-              <button 
-            //onClick={()=> signInWithGoogle()}
-            className='btn btn-light w-50 d-block mx-auto'>
-                <img style={{width: '30px'}} src={google} alt="" />
-                oogle Sign In
-            </button>
-        </div>
-    );
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
+  // error message
+  let errorElement;
+
+  if (error) {
+    errorElement=<div>
+      <p className='text-danger'>Error: {error?.message}</p>
+    </div>
+}
+
+
+
+if(user){
+    navigate (from, {replace: true});
+}
+    
+  return (
+    <div>
+      <div className="d-flex align-items-center">
+        <div style={{ height: "1px" }} className="bg-primary w-50"></div>
+        <p className="mt-2 px-2">or</p>
+        <div style={{ height: "1px" }} className="bg-primary w-50"></div>
+      </div>
+      {errorElement}
+      {/* set button */}
+      <button
+        onClick={()=> signInWithGoogle()}
+        className="btn btn-light w-50 d-block mx-auto"
+      >
+        <img style={{ width: "30px" }} src={google} alt="" />
+        oogle Sign In
+      </button>
+    </div>
+  );
 };
 
 export default SocialLogin;
